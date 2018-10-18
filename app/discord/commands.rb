9b7ad1f0d7d931d4fcdf567daa9aa8ee17d0af02
Elano Garcez <elanogarcez@gmail.com>
2018-10-18 17:19:59 -0300

    wip

--- app/discord/commands.rb
+++ app/discord/commands.rb
@@ -9,34 +9,6 @@ module Discord
       "pong! (#{Time.now - event.timestamp}s)"
     end
 
-    desc  = "Deletes X amount of messages from the channel"
-    usage = "#{configatron.discord.bot_prefix}prune <number>"
-    command :prune, description: desc, usage: usage, allowed_roles: @admin_roles do |event, amount|
-      amount = amount.to_i
-      return "You can only delete between 1 and 100 messages!" if amount > 100
-
-      event.channel.prune amount, true
-      event.respond "Done pruning #{amount} messages 💥"
-    end
-
-    desc  = "Sends a message to the specified channel"
-    usage = "#{configatron.discord.bot_prefix}say #channel some message"
-    command :say, min_args: 2, description: desc, usage: usage, allowed_roles: @admin_roles do |event, channel, *message|
-      channel = channel.gsub("<#", "").to_i
-      $bot.send_message channel, message.join(" ")
-    end
-
-    # This can be VERY dangerous in the wrong hands. Just allow the owner or very specific people to use it.
-    command :eval, help_available: false do |event, *code|
-      event.respond "Only the owner can do this" and break unless "#{event.user.id}" == configatron.discord.owner_id
-
-      begin
-        eval code.join(' ')
-      rescue => e
-        "Error 😭: ```#{e}```"
-      end
-    end
-
     # Meant to use locally only.
     command :debug, help_available: false do |event, *args|
       return "Nope!" unless configatron.app.env == "development"
---
+++ app/discord/rift/events.rb
@@ -0,0 +1,124 @@
+module Discord
+  module Rift
+    module Events
+      extend Discordrb::EventContainer
+
+      message(with_text: textAnySize("Chora Bixão"), in: 426072245538914316) do |event|
+        event.respond "Eu sei que tu chora, chora ai :clap: :clap: :clap:"
+      end
+
+      message(with_text: textAnySize("Da pra fazer?"), in: 426072245538914316) do |event|
+        event.respond "Dá demais"
+      end
+
+      message(with_text: textAnySize("Top Secret"), in: 426072245538914316) do |event|
+        event.respond "Isso é top secret cara, conta pra ninguém não"
+      end
+
+      message(with_text: textAnySize("Cidadão"), in: 426072245538914316) do |event|
+        event.respond "Olha ai Karlos o Cidadão"
+      end
+
+      message(with_text: textAnySize("testa ai"), in: 426072245538914316) do |event|
+        event.respond "Testa ai agora"
+      end
+
+      message(with_text: textAnySize("tchau"), in: 426072245538914316) do |event|
+        event.respond "tchaupatau"
+      end
+
+      message(with_text: textAnySize("skills"), in: 426072245538914316) do |event|
+        event.respond "tchaupatau"
+      end
+
+    end
+  end
+end
+
+def textAnySize text
+  # "hello James!".downcase    #=> "hello james!"
+  # "hello James!".upcase      #=> "HELLO JAMES!"
+  # "hello James!".capitalize  #=> "Hello james!"
+  # "hello James!".titleize    #=> "Hello James!"
+  [text, text.downcase, text.upcase, text.capitalize, text.titleize]
+end
+
+
+
+
+skills
+Last edited by schwarz
+
+
+
+tá duvidando das minhas skills?
+
+Ganhei
+Last edited by schwarz
+
+
+
+Eu ganhei foi???
+
+IPS
+Last edited by schwarz
+
+
+
+Karlos é que nem um laser, ele mira e atira.
+
+Tosco
+Last edited by schwarz
+
+
+
+Olha ai Karlos que tosco
+
+xuas punk
+Last edited by schwarz
+
+
+
+Ele é punk mesmo
+
+Stephen
+Last edited by schwarz
+
+
+
+Ei o Stephen é bom mesmo?
+
+apelido
+Last edited by schwarz
+
+
+
+Ei vocês sabiam que apelido dele é piroca
+
+Low Level
+Last edited by schwarz
+
+
+
+Isso é coisa de low level ein
+
+rachar
+Last edited by schwarz
+
+
+
+Ei mah vamo rachar?
+
+https://www.youtube.com/watch?v=2F46tGehnfo
+Last edited by schwarz
+
+
+
+Tá bom neh cidadão.
+
+repost
+Last edited by schwarz
+
+
+
+https://www.youtube.com/watch?v=2F46tGehnfo
