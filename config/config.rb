2b058ab9c3bb14d25adfe313828d74039ee08b5e
Elano Garcez <elanogarcez@gmail.com>
2018-10-22 01:27:29 -0300

    fix env

--- config/config.rb
+++ config/config.rb
@@ -1,4 +1,4 @@
-configatron.app.env            = ENV["APP_ENV"]     || "development"
+configatron.app.env            = ENV["RACK_ENV"]    || "development"
 configatron.app.disable_web    = ENV["DISABLE_WEB"] || false
 
 # Bot Opts
@@ -8,7 +8,3 @@ configatron.discord.bot_prefix = ENV["DISCORD_BOT_PREFIX"]
 configatron.discord.owner_id   = ENV["DISCORD_OWNER_ID"]
 configatron.discord.bot_game   = ENV["DISCORD_BOT_GAME"]
 configatron.discord.bot_mode   = configatron.app.disable_web ? nil : :async
-
-# Web Opts
-configatron.web.user           = ENV["WEB_USER"]
-configatron.web.password       = ENV["WEB_PASSWORD"]
