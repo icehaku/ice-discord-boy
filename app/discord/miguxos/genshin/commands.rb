8497ca62ebfa208f3c0d27f4829727d988446baa
Elano Garcez <elanogarcez@gmail.com>
2020-10-31 18:51:16 -0300

    genshin command improve

--- app/discord/miguxos/genshin/commands.rb
+++ app/discord/miguxos/genshin/commands.rb
@@ -9,10 +9,42 @@ module Discord
         extend Discordrb::Commands::CommandContainer
 
         command :genshin do |event|
-          event.respond "Lista"
-          event.respond "Uteis"
-        end
+          event.respond "Interactive Map"
+          event.respond "https://genshin-impact-map.appsample.com/#/"
 
+          event.respond "Artifact Role Guide"
+          event.respond "https://imgur.com/a/1lCe9Q9"
+
+          event.respond "Daily Tasks Guide"
+          event.respond "https://imgur.com/a/1lCe9Q9"
+
+          event.respond "Ascencion Material Guides"
+          event.respond "https://imgur.com/a/eVOqMWm"
+
+          event.respond "Cogumelo Route"
+          event.respond "https://imgur.com/a/MDewIxb"
+
+          event.respond "Daily Domain"
+          event.respond "https://imgur.com/a/bUJ6rJW"
+
+          event.respond "Element Reaction Guide"
+          event.respond "https://imgur.com/a/mkclMXG"
+
+          event.respond "Mining Routes"
+          event.respond "https://fragstrat.com/genshin-impact/best-mining-route-for-crystal-chunk"
+
+          event.respond "Star Conchs Route"
+          event.respond "https://www.reddit.com/r/Genshin_Impact/comments/jir3vr/you_can_get_4550_star_conchs_in_10_minutes_for/"
+
+          event.respond "Ascension Planner/Damage Calculator"
+          event.respond "https://genshin-center.com/"
+
+          event.respond "Elite Route"
+          event.respond "https://i.redd.it/fnyxvl22ytv51.png"
+
+          event.respond "Liyue Specialities Route"
+          event.respond "https://www.reddit.com/r/Genshin_Impact/comments/jjw2pe/liyue_100_specialities_farming_route_in_15_minutes/"
+        end
       end
     end
   end
